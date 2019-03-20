7c8a6b8af0a1e75b7e5c38df161d5d17a1aa2d99
romain <the.spartan.proj@gmail.com>
2019-03-20 01:41:04 -0300

    first release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ __license__ = "GNU GPL v3"
 __maintainer__ = "Romain Thomas"
 __email__ = "the.spartan.proj@gmail.com"
 __status__ = "released"
-__website__ = "https://astrom-tom.github.io/dfitspy/build/html/index.html"
+__website__ = "https://github.com/astrom-tom/stpip"
 
 setup(
    name = 'stpip',
